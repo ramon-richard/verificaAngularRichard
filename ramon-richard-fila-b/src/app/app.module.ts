930aea7016ee112890d55f3ea71c0d67f8cc36ea
ramon-richard <ramon.richard@iisgalvanimi.edu.it>
2021-11-26 12:27:35 +0000

    punto 2

--- ramon-richard-fila-b/src/app/app.module.ts
+++ ramon-richard-fila-b/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponentComponent,
+    ThirdComponentComponent,
+    FourthComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ ramon-richard-fila-b/src/app/fourth-component/fourth-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FourthComponentComponent } from './fourth-component.component';
+
+describe('FourthComponentComponent', () => {
+  let component: FourthComponentComponent;
+  let fixture: ComponentFixture<FourthComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FourthComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FourthComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
